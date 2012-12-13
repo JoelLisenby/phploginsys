6018280cabe57efdfe3c3497c28e982aecba59d6
Joel Lisenby <sven.svenagen@gmail.com>
2012-12-13 15:50:34 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,21 @@
 <?php
+/*
+This file is part of phploginsys.
+
+phploginsys is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+phploginsys is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with phploginsys.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 require_once "authentication/auth.php";
 $auth = new Authentication();
 
